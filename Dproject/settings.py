62bb1c538b763e9e5cfb58a2f73bf4059388833d
Hritik Suman <hritiksuman@gmail.com>
2020-07-01 21:46:52 +0530

    bug fixed

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,8 +126,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 CORS_ORIGIN_ALLOW_ALL = True
 
